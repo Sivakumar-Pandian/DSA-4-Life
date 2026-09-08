54f6cb863c1c8872b4d3df4e6ca949f09cb07ad7
Sivakumar Pandian <sivakumarpandian11@gmail.com>
2026-09-08 15:34:12 +0530

    files changed

---
+++ Arrays/6-Array-with-All-Palindromes.py
@@ -0,0 +1,31 @@
+"""
+Given an array arr[] of positive integers. Return true if all the array elements are palindrome otherwise, return false.
+
+Input: arr[] = [111, 222, 333, 444, 555]
+Output: true
+Explanation:
+arr[0] = 111, which is a palindrome number.
+arr[1] = 222, which is a palindrome number.
+arr[2] = 333, which is a palindrome number.
+arr[3] = 444, which is a palindrome number.
+arr[4] = 555, which is a palindrome number.
+As all numbers are palindrome so This will return true.
+
+Input: arr[] = [121, 131, 20]
+Output: false
+Explanation: 20 is not a palindrome hence the output is false.
+
+"""
+
+def paliArray(arr):
+
+    for i in arr:
+        val = str(i)
+        if val != val[::-1]:
+            return False
+        
+    return True
+
+arr = [111, 222, 333, 444,555]
+
+print(paliArray(arr))
